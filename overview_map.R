e304259e94ba4f35ca134e064aef06b9ca916e58
Felix Zeh <felix.zeh@gmx.de>
2022-07-06 09:50:44 +0200

    can read a gpx file and plot it in ggplot2

--- overview_map.R
+++ overview_map.R
@@ -1,2 +1,14 @@
 #creating an overview map of all visited sites
+library(ggplot2)
+library(ggmap)
+library(plotKML)
+library(openStreetmap)
+
+####importing GPX file####
+
+lst.rd <- readGPX('./gpx/test.gpx')
+df <- lst.rd$tracks[[1]][[1]]
+
+ggplot(df, aes(x=lon, y=lat))+
+  geom_point()
 
